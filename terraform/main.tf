8c475e5a60e079e6380518037da39d36e14ea9d0
prabhgill02 <prabhdeepsinghgill1@gmail.com>
2024-12-08 23:27:11 -0500

    changes ami

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-    ami = "ami-0c55b159cbfafe1f0"
+    ami = "ami-0b0ea68c435eb488d"
     instance_type = "t2.micro"
     count  = var.core_count
 
